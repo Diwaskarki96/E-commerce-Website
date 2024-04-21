c0e8a094814b3c913901ea7bad8e03643cd57ff0
Diwash Karki <diwaskarki96@gmail.com>
2024-04-21 23:34:33 +0545

    added page with navigation button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const navigate = useNavigate();
   return (
-    <>
+    <div>
+      <h3>Welcome to my Page</h3>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            navigate("/register");
+          }}
+        >
+          Register
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            navigate("/register");
+          }}
+        >
+          Login
+        </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
