81ce7faffa5b2bb1164d895463de36571aed5d57
Diwash Karki <diwaskarki96@gmail.com>
2024-04-24 13:36:11 +0545

    add axios interceptors

--- client/src/axios/axiosInstance.js
+++ client/src/axios/axiosInstance.js
@@ -4,3 +4,12 @@ export const $axios = axios.create({
   baseURL: "http://localhost:5555",
   timeout: 5000,
 });
+
+//axios request interceptor
+$axios.interceptors.request.use(function (config) {
+  const accessToken = localStorage.getItem("accessToken");
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
