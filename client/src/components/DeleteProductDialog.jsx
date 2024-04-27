c50f0bbaf910e0888965cc0d8d7b74cd5ee39cf5
Diwash Karki <diwaskarki96@gmail.com>
2024-04-27 09:29:45 +0545

    added delete dialog

---
+++ client/src/components/DeleteProductDialog.jsx
@@ -0,0 +1,85 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useNavigate, useParams } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { $axios } from "../axios/axiosInstance";
+import { CircularProgress } from "@mui/material";
+
+export default function DeleteProductDialog() {
+  const params = useParams();
+  const productId = params?.id;
+  const [open, setOpen] = React.useState(false);
+  const navigate = useNavigate();
+  const { isPending, mutate } = useMutation({
+    mutationKey: ["delete-product"],
+    mutationFn: async () => {
+      return await $axios.delete(`/product/delete/${productId}`);
+    },
+    onSuccess: () => {
+      navigate("/products");
+    },
+  });
+  if (isPending) {
+    return <CircularProgress />;
+  }
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button
+        variant="outlined"
+        color="error"
+        startIcon={<DeleteIcon />}
+        onClick={handleClickOpen}
+        fullWidth
+      >
+        Delete
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {`Are you sure you want to delete this product?`}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            This process is irreversible. Product is permanently deleted after
+            this action.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color="success" onClick={handleClose}>
+            No
+          </Button>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={() => {
+              mutate();
+              handleClose();
+            }}
+            autoFocus
+          >
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
