9f0679f7b27facba8f435fa3b0daba073699eb41
Diwash Karki <diwaskarki96@gmail.com>
2024-05-01 23:02:50 +0545

    complete pagination for products

--- client/src/components/BuyerProductList.jsx
+++ client/src/components/BuyerProductList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import ProductCard from "./ProductCard";
 import { Box, CircularProgress, Pagination } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
@@ -6,38 +6,51 @@ import { $axios } from "../axios/axiosInstance";
 import { fallBackImage } from "../constants/general.constants";
 
 const BuyerProductList = () => {
+  const [currentPage, setcurrentPage] = useState(1);
   const { isPending, data } = useQuery({
-    queryKey: ["get-buyer-products"],
+    queryKey: ["get-buyer-products", currentPage],
     queryFn: async () => {
       return await $axios.post("/product/productList/buyer", {
-        page: 1,
+        page: currentPage,
         limit: 3,
       });
     },
   });
+
   const productDetail = data?.data?.data;
+  const totalPage = data?.data?.totalPage;
   if (isPending) {
     return <CircularProgress />;
   }
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "3rem",
-      }}
-    >
-      {productDetail && productDetail.length > 0 ? (
-        productDetail.map((item) => {
-          return <ProductCard key={item._id} {...item} />;
-        })
-      ) : (
-        <img src={fallBackImage} alt="" />
-      )}
-      <Pagination count={10} color="secondary" />
-    </Box>
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "3rem",
+          mb: "2rem",
+        }}
+      >
+        {productDetail && productDetail.length > 0 ? (
+          productDetail.map((item) => {
+            return <ProductCard key={item._id} {...item} />;
+          })
+        ) : (
+          <img src={fallBackImage} alt="" />
+        )}
+      </Box>
+      <Pagination
+        page={currentPage}
+        count={totalPage}
+        color="secondary"
+        onChange={(_, value) => {
+          setcurrentPage(value);
+        }}
+      />
+    </>
   );
 };
 
--- client/src/components/SellerProductList.jsx
+++ client/src/components/SellerProductList.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
-import { Box, Button, CircularProgress } from "@mui/material";
+import { useState } from "react";
+import { Box, Button, CircularProgress, Pagination } from "@mui/material";
 import ProductCard from "./ProductCard";
 import { useNavigate } from "react-router-dom";
 import { $axios } from "../axios/axiosInstance";
@@ -8,59 +8,70 @@ import { fallBackImage } from "../constants/general.constants";
 
 const SellerProductList = () => {
   const navigate = useNavigate();
+  const [currentPage, setcurrentPage] = useState(1);
 
   const { isPending, data } = useQuery({
-    queryKey: ["get-seller-products"],
+    queryKey: ["get-seller-products", currentPage],
     queryFn: async () => {
       return await $axios.post("/product/productList/seller", {
-        page: 1,
+        page: currentPage,
         limit: 3,
       });
     },
   });
 
-  console.log(data);
   const productList = data?.data?.data;
-
+  const totalPage = data?.data?.totalPage;
   if (isPending) {
     return <CircularProgress />;
   }
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Button
-        variant="contained"
-        color="success"
-        onClick={() => {
-          navigate("/add-products");
-        }}
-        sx={{ marginBottom: "2rem", marginTop: "1rem" }}
-      >
-        add product
-      </Button>
+    <>
       <Box
         sx={{
           display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
+          flexDirection: "column",
           alignItems: "center",
-          gap: "3rem",
         }}
       >
-        {productList && productList.length > 0 ? (
-          productList.map((item) => {
-            return <ProductCard key={item._id} {...item} />;
-          })
-        ) : (
-          <img src={fallBackImage} alt="" />
-        )}
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() => {
+            navigate("/add-products");
+          }}
+          sx={{ marginBottom: "2rem", marginTop: "1rem" }}
+        >
+          add product
+        </Button>
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "3rem",
+            mb: "2rem",
+          }}
+        >
+          {productList && productList.length > 0 ? (
+            productList.map((item) => {
+              return <ProductCard key={item._id} {...item} />;
+            })
+          ) : (
+            <img src={fallBackImage} alt="" />
+          )}
+        </Box>
+        <Pagination
+          page={currentPage}
+          count={totalPage}
+          color="secondary"
+          onChange={(_, values) => {
+            setcurrentPage(values);
+          }}
+        />
       </Box>
-    </Box>
+    </>
   );
 };
 
