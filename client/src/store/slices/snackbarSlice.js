d2d0e8c90ee39befc36547afa651bc4c34ead8bd
Diwash Karki <diwaskarki96@gmail.com>
2024-05-07 23:26:28 +0545

    snackbar

---
+++ client/src/store/slices/snackbarSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const snackbarSlice = createSlice({
+  name: "counter",
+  initialState: {
+    open: "",
+    severity: "",
+    message: "",
+  },
+  reducers: {
+    openSuccessSnackbar: (state, action) => {
+      state.open = true;
+      state.severity = "success";
+      state.message = action.payload;
+    },
+    openErrorSnackbar: (state, action) => {
+      state.open = true;
+      state.severity = "error";
+      state.message = action.payload || "Something went wrong";
+    },
+    closeSnackbar: (state, action) => {
+      state.open = false;
+    },
+  },
+});
+
+export const { openSuccessSnackbar, openErrorSnackbar, closeSnackbar } =
+  snackbarSlice.actions;
+
+export default snackbarSlice.reducer;
