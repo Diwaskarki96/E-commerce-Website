a61bd274d90ff1f936cbb57655b95fc76dca15b8
Diwash Karki <diwaskarki96@gmail.com>
2024-06-28 17:15:00 +0545

    image slider

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,16 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
-import "../../style.css";
-// import required modules
-import { Parallax, Pagination, Navigation } from "swiper/modules";
+import "./slider.css";
+import { Parallax, Pagination, Navigation, Autoplay } from "swiper/modules";
+import TypeWriter from "../components/TypeWriter";
 
 export default function Home() {
   return (
     <>
+      <TypeWriter />
       <Swiper
         style={{
           "--swiper-navigation-color": "#fff",
@@ -24,8 +21,12 @@ export default function Home() {
         pagination={{
           clickable: true,
         }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
         navigation={true}
-        modules={[Parallax, Pagination, Navigation]}
+        modules={[Parallax, Pagination, Navigation, Autoplay]}
         className="mySwiper"
       >
         <div
@@ -33,66 +34,58 @@ export default function Home() {
           className="parallax-bg"
           style={{
             height: "400px",
-
-            "background-image":
+            backgroundImage:
               "url(https://images.unsplash.com/photo-1629131726692-1accd0c53ce0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzV8fGxhcHRvcHxlbnwwfDB8MHx8fDA%3D)",
           }}
           data-swiper-parallax="-23%"
         ></div>
         <SwiperSlide>
           <div className="title" data-swiper-parallax="-300">
-            Slide 1
+            Introducing the M3 Air Laptop
           </div>
           <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
+            The Future of Portable Computing
           </div>
           <div className="text" data-swiper-parallax="-100">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+              {`The M3 Air Laptop is a game-changer in the world of portable
+              computing. With its sleek design, powerful performance, and
+              unmatched portability, it's the perfect companion for both work
+              and play.`}
             </p>
           </div>
         </SwiperSlide>
+
         <SwiperSlide>
           <div className="title" data-swiper-parallax="-300">
-            Slide 2
+            Unmatched Performance
           </div>
           <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
+            Power at Your Fingertips
           </div>
           <div className="text" data-swiper-parallax="-100">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+              {`The M3 Air Laptop's high-resolution display offers stunning
+              visuals, while the advanced M3 processor ensures smooth
+              multitasking. Experience the power and performance like never
+              before.`}
             </p>
           </div>
         </SwiperSlide>
+
         <SwiperSlide>
           <div className="title" data-swiper-parallax="-300">
-            Slide 3
+            Long-lasting Battery Life
           </div>
           <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
+            Stay Productive All Day
           </div>
           <div className="text" data-swiper-parallax="-100">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+              {`The M3 Air Laptop boasts a long-lasting battery, so you can stay
+              productive all day. Whether you're a student, a professional, or a
+              digital nomad, the M3 Air Laptop is designed to keep up with your
+              on-the-go lifestyle.`}
             </p>
           </div>
         </SwiperSlide>
