3dd925ae51d63f30274a458daa64eee0306ae860
Diwash Karki <diwaskarki96@gmail.com>
2024-06-30 19:21:35 +0545

    snackbar in cart

--- client/src/components/CartItemTable.jsx
+++ client/src/components/CartItemTable.jsx
@@ -25,23 +25,20 @@ import ClearIcon from "@mui/icons-material/Clear";
 import { fallBackImage } from "../constants/general.constants";
 import { useNavigate, useParams } from "react-router-dom";
 import { Add } from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+import {
+  openErrorSnackbar,
+  openSuccessSnackbar,
+} from "../store/slices/snackbarSlice";
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 const CartItemTable = ({ cartData }) => {
+  const dispatch = useDispatch();
   const queryClient = useQueryClient();
   const navigate = useNavigate();
-  console.log(cartData);
   const {
     isPending: removeSingleCartItemPending,
     mutate: removeSingleCartMutate,
@@ -50,8 +47,12 @@ const CartItemTable = ({ cartData }) => {
     mutationFn: async (productId) => {
       return await $axios.delete(`/cart/delete/${productId}`);
     },
-    onSuccess: () => {
+    onSuccess: (res) => {
       queryClient.invalidateQueries("get-cart-item-list");
+      dispatch(openSuccessSnackbar(res?.data?.message));
+    },
+    onError: (error) => {
+      dispatch(openErrorSnackbar(error?.response?.data));
     },
   });
   const { isPending: removeAllCartPending, mutate: removeAllCartMutate } =
